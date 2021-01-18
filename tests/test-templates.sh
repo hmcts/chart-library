e9a87974036e9816447ee2b014cc23dd39e9ff26
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-01-18 12:45:51 +0000

    try diff

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -10,7 +10,7 @@ yq eval -i '.type = "application"' library/Chart.yaml
 for file in $(echo "hpa.yaml secretproviderclass.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
   cp tests/$file library/templates/
   helm template library -f ci-values.yaml > template-$file
-  yq compare template-$file tests/results/template-$file
+  diff template-$file tests/results/template-$file
   rm -rf library/templates/$file template-$file
 done
 
