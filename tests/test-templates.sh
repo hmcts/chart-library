f765df6cb2cbd80e29a3a83bda2fe39b5a47878e
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-01-20 12:01:06 +0000

    sort keys

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -11,6 +11,11 @@ for version in $(echo "v1 v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
     helm template library -f ci-values.yaml > $file
+    
+    # sort yamls before compare
+    yq eval -i 'sortKeys(..)' $file
+    yq eval -i 'sortKeys(..)' tests/results/$file
+    
     diff -w $file tests/results/$file
     rm -rf library/templates/$file $file
   done
