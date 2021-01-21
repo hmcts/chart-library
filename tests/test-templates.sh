dd6cceb9faaee599cf395c07e77b9251ac0ae1cf
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-01-21 09:48:27 +0000

    Renaming files

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -8,7 +8,7 @@ helm lint library/
 yq w -i library/Chart.yaml type application
 
 for file in $(echo "hpa.yaml secretproviderclass.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
-  cp tests/$file library/templates/
+  cp tests/v1/$file library/templates/
   helm template library -f ci-values.yaml > template-$file
   yq compare template-$file tests/results/template-$file
   rm -rf library/templates/$file template-$file
