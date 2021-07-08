fa64e467415e501472e1836b205832b32a5dc1a1
Enda Kelly <enda.kelly@hmcts.net>
2021-07-08 15:17:35 +0100

    Reverting -w and adding echo to sa.yaml

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -11,7 +11,8 @@ for version in $(echo "v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
     helm template release-name library -f ci-values.yaml > $file
-    diff $file tests/results/$file
+    echo $file
+    diff -w $file tests/results/$file
     
     #Language specific test
     helm template release-name library -f ci-values-lang.yaml > $file
