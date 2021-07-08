74d4b62f2078c85f7d4b91d0afce43ee05faffb4
Enda Kelly <enda.kelly@hmcts.net>
2021-07-08 09:55:39 +0100

    Adding debug flag to script for testing

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -10,11 +10,11 @@ yq eval -i '.type = "application"' library/Chart.yaml
 for version in $(echo "v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
-    helm template release-name library -f ci-values.yaml > $file
+    helm template --debug release-name library -f ci-values.yaml > $file
     diff -w $file tests/results/$file
     
     #Language specific test
-    helm template release-name library -f ci-values-lang.yaml > $file
+    helm template --debug release-name library -f ci-values-lang.yaml > $file
     diff -w $file tests/results/$file
     
     rm -rf library/templates/$file $file
