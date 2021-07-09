aed0adc4600c9212a9ad5296f8b820f98ca84b92
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2021-07-09 09:57:16 +0100

    Update test-templates.sh

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -11,7 +11,6 @@ for version in $(echo "v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
     helm template release-name library -f ci-values.yaml > $file
-    echo $file
     diff -w $file tests/results/$file
     
     #Language specific test
