c1dd3f3341461c96283ee87cac822d424d39be28
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-01-13 15:16:44 +0000

    debug

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -10,12 +10,14 @@ yq eval -i '.type = "application"' library/Chart.yaml
 for version in $(echo "v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
-    echo "checking $file"
+    echo "#####Checking $file"
     helm template release-name library -f ci-values.yaml > $file
+    cat $file
     diff -w $file tests/results/$file
     
     #Language specific test
     helm template release-name library -f ci-values-lang.yaml > $file
+    cat $file
     diff -w $file tests/results/$file
 
     rm -rf library/templates/$file $file
