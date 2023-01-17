3c4129ecbc1a065086252cb79b1400a7dcd15258
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-01-17 10:13:06 +0000

    revert_debug

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -12,12 +12,10 @@ for version in $(echo "v2"); do
     cp tests/$version/$file library/templates/
     echo "checking $file"
     helm template release-name library -f ci-values.yaml > $file
-    cat $file
     diff -w $file tests/results/$file
     
     #Language specific test
     helm template release-name library -f ci-values-lang.yaml > $file
-    cat $file 
     diff -w $file tests/results/$file
 
     rm -rf library/templates/$file $file
