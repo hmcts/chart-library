bf039d66fcaab57fac63b960a24377ab414e8a62
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-01-17 10:07:17 +0000

    debug

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -11,11 +11,15 @@ for version in $(echo "v2"); do
   for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml sa.yaml"); do
     cp tests/$version/$file library/templates/
     echo "checking $file"
+    cat $file
+    echo
     helm template release-name library -f ci-values.yaml > $file
     diff -w $file tests/results/$file
     
     #Language specific test
     helm template release-name library -f ci-values-lang.yaml > $file
+    cat $file 
+    echo
     diff -w $file tests/results/$file
 
     rm -rf library/templates/$file $file
