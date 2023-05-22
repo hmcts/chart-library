c360bde3edced4bb2207b651e8198778a43d18ea
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2023-05-22 15:59:13 +0100

    deleted old tests, updated script and updated unit test

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -2,25 +2,16 @@
 
 set -e
 
-helm lint library/
+helm lint library/ --values ci-values.yaml
 
 #change library chart to application
 yq eval -i '.type = "application"' library/Chart.yaml
 
-for version in $(echo "v2"); do
-  for file in $(echo "hpa.yaml secretproviderclass.yaml secretproviderclass-tests.yaml deployment.yaml configmap.yaml ingress.yaml pdb.yaml service.yaml deployment-tests.yaml"); do
-    cp tests/$version/$file library/templates/
-    echo "checking $file"
-    helm template release-name library -f ci-values.yaml > $file
-    diff <(yq -P 'sort_keys(..)' $file) <(yq -P 'sort_keys(..)' tests/results/$file)
-    
-    #Language specific test
-    helm template release-name library -f ci-values-lang.yaml > $file
-    diff <(yq -P 'sort_keys(..)' $file) <(yq -P 'sort_keys(..)' tests/results/$file)
+helm unittest -v ci-values.yaml library -q -f 'tests/snapshot-tests/*.yaml'
+helm unittest -v ci-values-lang.yaml library -q -f 'tests/snapshot-tests/*.yaml'
 
-    rm -rf library/templates/$file $file
-  done
-done
+helm unittest -v ci-values.yaml library -q -f 'tests/unit-tests/*.yaml'
+helm unittest -v ci-values-lang.yaml library -q -f 'tests/unit-tests/*.yaml'
 
 #revert test changes
 yq eval -i '.type = "library"' library/Chart.yaml
