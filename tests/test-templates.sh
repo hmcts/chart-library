3d0ab3bb51eb1cb9fe707027e9b2b9c0fd65db45
Rees Pozzi <reespozzi@icloud.com>
2023-05-26 13:33:20 +0100

    Debug helm unittest command

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -3,15 +3,16 @@
 set -e
 
 helm lint library/ --values ci-values.yaml
+helm unittest --help
 
 #change library chart to application
 yq eval -i '.type = "application"' library/Chart.yaml
 
-helm unittest -v ci-values.yaml library -q -f 'tests/snapshot-tests/*.yaml'
-helm unittest -v ci-values-lang.yaml library -q -f 'tests/snapshot-tests/*.yaml'
+helm unittest --values ci-values.yaml library -q -f 'tests/snapshot-tests/*.yaml'
+helm unittest --values ci-values-lang.yaml library -q -f 'tests/snapshot-tests/*.yaml'
 
-helm unittest -v ci-values.yaml library -q -f 'tests/unit-tests/*.yaml'
-helm unittest -v ci-values-lang.yaml library -q -f 'tests/unit-tests/*.yaml'
+helm unittest --values ci-values.yaml library -q -f 'tests/unit-tests/*.yaml'
+helm unittest --values ci-values-lang.yaml library -q -f 'tests/unit-tests/*.yaml'
 
 #revert test changes
 yq eval -i '.type = "library"' library/Chart.yaml
