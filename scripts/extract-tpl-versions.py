49cde726f8df6289b7989e8cb28e78ffc836e911
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-08-14 13:57:18 +0100

    DTSPO-18260 - Add script to extract template versions (#125)
    * Add script to extract template versions
    
    * Move file

---
+++ scripts/extract-tpl-versions.py
@@ -0,0 +1,38 @@
+import re
+import glob
+import os
+
+def find_and_extract_pattern(file_path, pattern):
+    # Read the content of the file
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Find all matches of the pattern in the content
+    matches = pattern.findall(content)
+
+    # Write the matches to a temporary text file
+    with open('/tmp/master-tpl-versions.txt', 'a') as temp_file:
+        for match in matches:
+            temp_file.write(match + '\n')
+    
+    # Read the content of the temporary text file
+    with open('/tmp/master-tpl-versions.txt', 'r') as temp_file:
+        content = temp_file.readlines()
+
+    # Remove the .tpl suffix and duplicates from the content
+    cleaned_content = list(set([line.strip().replace('.tpl', '') for line in content]))
+
+    # Write the cleaned content back to the temporary text file
+    with open('/tmp/master-tpl-versions.txt', 'w') as temp_file:
+        for line in cleaned_content:
+            temp_file.write(line + '\n')
+
+# Define the regex pattern to find the template
+pattern = re.compile(r'define\s+"([^"]+)"')
+
+# Get a list of all files in the repository
+all_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('./library/templates') for f in filenames]
+
+# Iterate through each file and find and extract the pattern
+for file_path in all_files:
+    find_and_extract_pattern(file_path, pattern)
