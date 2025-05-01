3da03447cd7438f9d6928c9fad8e1bf8e763e9bf
Marty <9006614+MartyFox@users.noreply.github.com>
2025-05-01 10:27:46 +0100

    Update test file and unit test version to comply with working structure (#130)
    * Move ci-values* files into library folder to comply with Unit Test update, update test script and remove version pin for Helm Unit Test plugin

--- tests/test-templates.sh
+++ tests/test-templates.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-helm lint library/ --values ci-values.yaml
+helm lint library/ --values library/ci-values.yaml
 helm unittest --help
 
 #change library chart to application
 yq eval -i '.type = "application"' library/Chart.yaml
 
-helm unittest --values ci-values.yaml library -q -f 'tests/snapshot-tests/*.yaml'
-helm unittest --values ci-values-lang.yaml library -q -f 'tests/snapshot-tests/*.yaml'
+# Snapshot tests for library chart
+helm unittest --values library/ci-values.yaml library -q -f 'tests/snapshot-tests/*.yaml'
+helm unittest --values library/ci-values-lang.yaml library -q -f 'tests/snapshot-tests/*.yaml'
 
-helm unittest --values ci-values.yaml library -q -f 'tests/unit-tests/*.yaml'
-helm unittest --values ci-values-lang.yaml library -q -f 'tests/unit-tests/*.yaml'
+# Unit tests for library chart
+helm unittest --values library/ci-values.yaml library -q -f 'tests/unit-tests/*.yaml'
+helm unittest --values library/ci-values-lang.yaml library -q -f 'tests/unit-tests/*.yaml'
 
 #revert test changes
 yq eval -i '.type = "library"' library/Chart.yaml
